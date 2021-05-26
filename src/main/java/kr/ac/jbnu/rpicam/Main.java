feeda1dbf33a48deeefbbb889f7fc0aa4e86dcb8
bogyung99 <bogung1207@naver.com>
2021-05-26 07:20:39 -0400

    opencv

--- src/main/java/kr/ac/jbnu/rpicam/Main.java
+++ src/main/java/kr/ac/jbnu/rpicam/Main.java
@@ -12,6 +12,10 @@ import kr.ac.jbnu.rpicam.exceptions.FailedToRunRaspistillException;
 import java.io.IOException;
 import java.util.Scanner;
 
+// import org.opencv.core.Core;
+// import org.opencv.core.Cytype;
+// import org.opencv.core.Mat;
+
 public class Main {
 
     public static void main(String[] args) {
